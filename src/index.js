dd167c79e16e6d3e706267c9e8fcbb2a9b70fe36
Tushar Budhiraja <tushar02@centilytics.com>
2023-11-30 00:20:57 +0530

    user router and register done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import dotenv from 'dotenv';
-import Connection_DB from './db/dbConnection.js';
-
+import dotenv from "dotenv";
+import Connection_DB from "./db/dbConnection.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
-
-Connection_DB().then(()=>{
-    app.listen(process.env.PORT || 8000 , ()=>{
-        console.log(`Server is listening at port no.->  ${process.env.PORT}`)
-    })
-}).catch((error)=>{` Error file connecting to mongodb database-> ${error}`})
+Connection_DB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is listening at port no.->  ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    ` Error file connecting to mongodb database-> ${error}`;
+  });
--- src/utilis/asyncHandler.js
+++ src/utilis/asyncHandler.js
@@ -2,8 +2,8 @@
 //  --> Making a async handker function with help of promises
 
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).reject((error) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) => {
       next(error);
       res.status(error.code || 500).json({
         success: false,
